262ec9bdaddcc9414b74b51062f73280018915ff
Auxilor <william.favierparsons1@gmail.com>
2022-01-23 12:49:28 +0000

    Set up page layout

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Head from 'next/head'
 import ThemeToggle from './ThemeToggle'
 import NavbarLink from "./NavbarLink";
+import Image from 'next/image'
 
 const PageWrapper: FC<{}> = props => {
     return (
@@ -10,29 +11,52 @@ const PageWrapper: FC<{}> = props => {
                 <title>Will FP</title>
             </Head>
 
-            <div className='container mx-auto px-4 py-4'>
-                <div className='px-10 py-10'>
-                    <a href="../">
-                        <h1 className='text-5xl font-sans font-bold'>I&apos;m Will FP.</h1>
-                    </a>
-                    <h3 className='text-2xl font-sans font-extralight'>I&apos;m a full-stack developer.</h3>
+            <div className='container mx-auto pl-4 py-4 flex flex-col'>
+                <div className="basis-1/4">
+                    <div className='pl-10 pt-10 flex flex-row'>
+                        <div className="mt-2">
+                            <Image
+                                width={64}
+                                height={64}
+                                className="object-contain w-64 h-64 rounded-full relative"
+                                src={require('../styles/assets/auxilor.jpeg')}
+                                alt="Auxilor Logo"
+                            />
+                        </div>
+                        <div className='ml-6 flex flex-col'>
+                            <a href="../">
+                                <h1 className='text-5xl font-sans font-bold'>I&apos;m Will FP.</h1>
+                            </a>
+                            <h3 className='text-2xl font-sans font-extralight'>I&apos;m a full-stack developer.</h3>
+                        </div>
+                    </div>
                 </div>
 
-                <div className='px-10'>
-                    <ul>
-                        <li>
-                            <NavbarLink name="Projects" link="projects" />
-                        </li>
-                        <li>
-                            <NavbarLink name="Contact" link="contact" />
-                        </li>
-                        <li>
-                            <ThemeToggle />
-                        </li>
-                    </ul>
-                </div>
+                <div className='pl-10 py-4 basis-3/4'>
+                    <div className='container mx-auto py-4 flex flex-row'>
+                        <div className='basis-1/12'>
+                            <ul>
+                                <li>
+                                    <NavbarLink name="Home" link="../" />
+                                </li>
+                                <li>
+                                    <NavbarLink name="Projects" link="projects" />
+                                </li>
+                                <li>
+                                    <NavbarLink name="Contact" link="contact" />
+                                </li>
+                                <li>
+                                    <ThemeToggle />
+                                </li>
+                            </ul>
+                        </div>
 
-                {props.children}
+                        <div className='ml-10 basis-11/12'>
+                            {props.children}
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div >
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,10 @@ import PageWrapper from '../components/PageWrapper'
 const Home: NextPage = () => {
   return (
     <PageWrapper>
-
+      <h1 className='text-3xl font-sans font-extrabold'>Amongus</h1>
+      <p className='font-sans font-light'>
+        I&apos;m 
+      </p>
     </PageWrapper>
   )
 }
